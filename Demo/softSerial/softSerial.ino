da861952ca0606c2af91b5e29431af9e8d17c824
hzf <888iagiag888@gmail.com>
2023-07-21 06:47:28 +0800

    add soft serial code

---
+++ Demo/softSerial/softSerial.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(12, 13); // RX, TX
+void setup()
+{
+  softSerial.begin(9600);
+  Serial.begin(9600);
+}
+void loop()
+{
+  digitalWrite(53, LOW);
+  if (softSerial.available())
+  { 
+    // 至少得delay 200 ms
+    // 在openmv中最好也sleep_ms 10
+    delay(200); 
+    if (softSerial.read() == 's')
+    {
+      Serial.print("fuck");
+    }
+  }
+}
